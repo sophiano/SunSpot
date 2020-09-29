f3e60e7cb6aae5c7d7653f161212a897e9229f0a
sophiano <34742930+sophiano@users.noreply.github.com>
2020-09-29 10:18:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    data_files=[('', ['data/Ns.txt'])],
+    data_files=[('', ['data/Ns'])],
     include_package_data=True,
-    package_data={'': ['data/Ns.txt']},
+    package_data={'': ['data/Ns']},
 )
