228ca4a619640d94b094f563dc1e288759732cd3
sophiano <34742930+sophiano@users.noreply.github.com>
2020-09-30 08:57:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
 
-with open("README.txt", "r") as fh:
-    long_description = fh.read()
+#with open("README.txt", "r") as fh:
+#    long_description = fh.read()
+    
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.txt')) as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name = "SunSpot", 
-    version = "0.0.1",
-    author = "Sophie Mathieu",
-    author_email = "sph.mathieu@gmail.com",
-    description = "Non parametric robust monitoring of sunspot data",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/sophiano/SunSpot",
-    packages = setuptools.find_packages(),
+    name="SunSpot", 
+    version="0.0.1",
+    author="Sophie Mathieu",
+    author_email="sph.mathieu@gmail.com",
+    description="Non parametric robust monitoring of sunspot data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sophiano/SunSpot",
+    packages=setuptools.find_packages(),
     install_requires=['numpy>=1.19.1', 
                       'scipy>=1.5.2',
                       'matplotlib>=3.3.1',
@@ -20,7 +25,7 @@ setuptools.setup(
                       'pandas>=1.1.2',
                       'statsmodels>=0.10.1'
                       ],
-    classifiers = [
+    classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
