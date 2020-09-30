8bbdd46e72da9eb2a4dad075839fab3641855953
sophiano <34742930+sophiano@users.noreply.github.com>
2020-09-30 08:57:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setuptools.setup(
     python_requires='>=3.6',
     #data_files=[('', ['data/Ns'])],
     include_package_data=True,
-    package_data={'': ['data/*']},
+    #package_data={'': ['data/*']},
 )
